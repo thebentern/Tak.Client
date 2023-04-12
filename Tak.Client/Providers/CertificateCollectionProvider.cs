17af68d037f7c58ba938999bd30128bbc16db9b0
Ben Meadors <benmmeadors@gmail.com>
2023-04-12 15:59:55 -0500

    Whitespace

--- Tak.Client/Providers/CertificateCollectionProvider.cs
+++ Tak.Client/Providers/CertificateCollectionProvider.cs
@@ -10,6 +10,7 @@ internal class CertificateCollectionProvider
     private const string PrefsKey = "com.atakmap.app_preferences";
     private const string CertLocationKey = "certificateLocation";
     private const string CertPasswordKey = "clientPassword";
+
     private readonly string packagePath;
 
     public CertificateCollectionProvider(string packagePath)
@@ -22,6 +23,7 @@ internal class CertificateCollectionProvider
         using var package = new ZipArchive(new FileStream(packagePath, FileMode.Open));
         var pref = manifest.Preference.First(p => p.Name == PrefsKey);
         var certFileName = Path.GetFileName(pref.Entry.First(e => e.Key == CertLocationKey).Text);
+
         using var certStream = new MemoryStream();
         var certEntry = package.Entries.First(e => e.Name.Contains(certFileName));
         certEntry.Open().CopyToAsync(certStream);
